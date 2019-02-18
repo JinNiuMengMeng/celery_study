3123b1c22d5450109b1a814d306896087c93c330
XieZhaoheng <411317572@qq.com>
2019-02-18 09:51:23 +0800

    添加文件

--- imooc/manage.py
+++ imooc/manage.py
@@ -15,4 +15,9 @@ if __name__ == "__main__":
 Install: pip install django-celery
 项目启动: python manage.py runserver
 Worker: python manage.py celery worker --loglevel=info
+
+监控工具
+Install: pip install flower==0.9.0
+Start: celery flower --address=0.0.0.0 --port=5555 --broker=xxx --basic_auth=imooc:imooc
+或者使用 python manage.py celery flower # 从manage.py配置文件中读取配置信息
 """
